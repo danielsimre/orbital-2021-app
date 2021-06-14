bcf682daf58d6ac98bf14e82f597c4a1c5ca7285
Wee Han <weehanng@yahoo.com>
2021-06-14 19:44:38 +0800

    Revert "feat: added backend support for tasks and comments (temp)"
    This reverts commit 6bec9ebce9835689424e369df30f45063bcb1d72.

--- client/src/components/NewProjectForm/NewProjectForm.js
+++ client/src/components/NewProjectForm/NewProjectForm.js
@@ -15,6 +15,7 @@ function NewProjectForm(props) {
   );
   const [hasDateError, setHasDateError] = useState(false);
   const [errorText, setErrorText] = useState("");
+  
 
   // for the alert
   const [displayAlert, setDisplayAlert] = useState(false);
@@ -74,56 +75,56 @@ function NewProjectForm(props) {
   }
 
   return (
-    <>
-      <form onSubmit={handleSubmit} className={styles.newProjectForm}>
-        <fieldset>
-          <legend>Create New Project</legend>
-          <div>
-            <TextField
-              id="project_name"
-              label="Project Name"
-              variant="outlined"
-              required
-              value={projName}
-              onChange={(event) => setProjName(event.target.value)}
-            />
-          </div>
-          <div>
-            <TextField
-              id="project_descrption"
-              label="Description"
-              variant="outlined"
-              multiline
-              required
-              value={projDescription}
-              onChange={(event) => setProjDescription(event.target.value)}
-            />
-          </div>
-          <div>
-            <TextField
-              id="due_date"
-              label="Due Date"
-              type="date"
-              required
-              value={projDueDate}
-              onChange={(event) => setProjDueDate(event.target.value)}
-              InputLabelProps={{
-                shrink: true,
-              }}
-              error={hasDateError}
-              helperText={errorText}
-            />
-          </div>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            style={{ margin: "0 auto", display: "flex" }}
-          >
-            Create New Project
-          </Button>
-        </fieldset>
-      </form>
+  <>
+    <form onSubmit={handleSubmit} className={styles.newProjectForm}>
+      <fieldset>
+        <legend>Create New Project</legend>
+        <div>
+          <TextField
+            id="project_name"
+            label="Project Name"
+            variant="outlined"
+            required
+            value={projName}
+            onChange={(event) => setProjName(event.target.value)}
+          />
+        </div>
+        <div>
+          <TextField
+            id="project_descrption"
+            label="Description"
+            variant="outlined"
+            multiline
+            required
+            value={projDescription}
+            onChange={(event) => setProjDescription(event.target.value)}
+          />
+        </div>
+        <div>
+          <TextField
+            id="due_date"
+            label="Due Date"
+            type="date"
+            required
+            value={projDueDate}
+            onChange={(event) => setProjDueDate(event.target.value)}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            error={hasDateError}
+            helperText={errorText}
+          />
+        </div>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          style={{ margin: "0 auto", display: "flex" }}
+        >
+          Create New Project
+        </Button>
+      </fieldset>
+    </form>
       <div>
         {displayAlert && (
           <Alert
--- models/Task.js
+++ models/Task.js
@@ -2,57 +2,31 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const TaskSchema = new Schema(
-  {
-    groupId: {
-      // group the task belongs to
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Group",
-      required: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    desc: {
-      type: String,
-      required: true,
-    },
-    dueDate: {
-      type: Date,
-      required: true,
-    },
-    submissions: {
-      type: [String],
-    },
-    assignedTo: {
-      // users tasked to complete this task
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    isCompleted: {
-      type: Boolean,
-      required: true,
-      default: false,
-    },
-    isMilestone: {
-      type: Boolean,
-      default: false,
-    },
+const TaskSchema = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
   },
-  {
-    toJSON: {
-      virtuals: true,
-      transform: (doc, ret) => {
-        const { id, ...rest } = ret;
-        delete rest._id;
-        delete rest.__v;
-        return { id: ret.id, attributes: { ...rest } };
-      },
-    },
-  }
-);
+  projectId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Project",
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  isCompleted: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  dueDate: {
+    type: Date,
+    required: true,
+  },
+});
 
 const Task = mongoose.model("Task", TaskSchema);
 
