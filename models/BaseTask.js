08a3e9ed9ba29742ed5b8580b4e78a61253a9e90
Wee Han <weehanng@yahoo.com>
2021-06-15 16:12:38 +0800

    Add base options for proper discrimination on BaseTask

--- models/BaseTask.js
+++ models/BaseTask.js
@@ -2,6 +2,24 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
+// tasks can be discriminated by their taskType (in baseOptions)
+// if not can query the tasks collection for all tasks instead
+// see: https://stackoverflow.com/questions/38725454/mongoose-discriminators-behavior
+
+const baseOptions = {
+  discriminatorKey: "taskType",
+  collection: "task",
+  toJSON: {
+    virtuals: true,
+    transform: (doc, ret) => {
+      const { id, ...rest } = ret;
+      delete rest._id;
+      delete rest.__v;
+      return { id: ret.id, type: "tasks", attributes: { ...rest } };
+    },
+  },
+};
+
 const BaseTaskSchema = new Schema(
   {
     name: {
@@ -34,25 +52,15 @@ const BaseTaskSchema = new Schema(
       default: false,
     },
   },
-  {
-    toJSON: {
-      virtuals: true,
-      transform: (doc, ret) => {
-        const { id, ...rest } = ret;
-        delete rest._id;
-        delete rest.__v;
-        return { id: ret.id, type: "tasks", attributes: { ...rest } };
-      },
-    },
-  }
+  baseOptions
 );
 
-const Task = mongoose.model("Task", BaseTaskSchema);
+const BaseTask = mongoose.model("BaseTask", BaseTaskSchema);
 
 // 2nd parameter is another schema: discriminator returns a union of both schemas provided
-const ParentTask = Task.discriminator(
+const ParentTask = BaseTask.discriminator(
   "ParentTask",
-  new mongoose.Schema({ subtasks: [Task] })
+  new mongoose.Schema({ subtasks: [BaseTask] })
 );
 
-export { Task, ParentTask };
+export { BaseTask, ParentTask };
