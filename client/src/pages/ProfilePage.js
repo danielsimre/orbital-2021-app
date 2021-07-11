185538877fc91b57e17747ccf36a2ef1118db139
Wee Han <weehanng@yahoo.com>
2021-07-11 17:03:15 +0800

    feat: add basic profile page

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,10 +1,33 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Typography } from "@material-ui/core";
+import {
+  Button,
+  Card,
+  CardContent,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+
+const useStyles = makeStyles({
+  container: {
+    width: "90%",
+    margin: "auto",
+    display: "flex",
+  },
+  card: {
+    width: "50%",
+  },
+  header: {
+    textAlign: "center",
+    padding: "1rem",
+  },
+});
 
 function ProfilePage(props) {
   const [curUserInfo, setCurUserInfo] = useState({});
   const [isRetrieving, setIsRetrieving] = useState(true);
+
+  const classes = useStyles();
 
   function getUserData() {
     // GET request
@@ -23,7 +46,29 @@ function ProfilePage(props) {
   return (
     isRetrieving || (
       <div>
-        <Typography>Username: {curUserInfo.username}</Typography>
+        <Typography variant="h4" className={classes.header}>
+          Profile Page
+        </Typography>
+        <div className={classes.container}>
+          <Card variant="outlined" className={classes.card}>
+            <Typography variant="h5" className={classes.header}>
+              User Info
+            </Typography>
+            <CardContent>
+              <Typography>Username: {curUserInfo.username}</Typography>
+              <Typography>Email: {curUserInfo.email}</Typography>
+            </CardContent>
+          </Card>
+          <Card variant="outlined" className={classes.card}>
+            <Typography variant="h5" className={classes.header}>
+              Settings
+            </Typography>
+            <CardContent>
+              <Button>Change Username</Button>
+              <Button>Change Password</Button>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     )
   );
